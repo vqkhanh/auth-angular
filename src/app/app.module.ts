091bcd65f6ae0c6224ee03b2ae1fddb7235e8721
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-06-08 07:24:28 +0700

    init auth angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login/login.component';
+import { NoAccessComponent } from './no-access/no-access/no-access.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
+import { SignupComponent } from './signup/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent,
+    NoAccessComponent,
+    NotFoundComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
